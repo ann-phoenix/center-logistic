b0ad453884643128c491f4caeee9a877888c2139
ann-phoenix <ann.phoenix.it@gmail.com>
2020-08-24 00:11:29 +0300

    added jq header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,27 @@
 $(function(){
 
+  //header
+  let intro = $('#intro');
+  let header = $('#header');
+  let introH = intro.innerHeight();
+  let headerH = header.innerHeight();
 
+  console.log(introH, headerH);
 
-  
+  $(window).on('scroll resize', function() {
+    headerScroll();
+ });
+
+ function headerScroll(){
+	  introH = intro.innerHeight();
+	  headerH = header.innerHeight();
+	  
+	  let scrollTop	= $(this).scrollTop();
+	  		
+	  if(scrollTop >= (introH - headerH)) {
+	  	header.addClass('header--dark');
+	  } else {
+	  	header.removeClass('header--dark');
+	  }
+  }
 });
