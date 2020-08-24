5f2838fbea4242a13b197d62cd08b7b450f1abee
ann-phoenix <ann.phoenix.it@gmail.com>
2020-08-24 20:32:35 +0300

    added smooth scroll to sections and scrollspy jq

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,16 @@
 $(function(){
 
-  //header
   let intro = $('#intro');
   let header = $('#header');
   let introH = intro.innerHeight();
   let headerH = header.innerHeight();
+  let scrollTop = $(window).scrollTop();
 
-  console.log(introH, headerH);
 
-  $(window).on('scroll resize', function() {
+  /* Header class on scroll */
+ headerScroll();
+
+ $(window).on('scroll resize', function() {
     headerScroll();
  });
 
@@ -24,4 +26,48 @@ $(function(){
 	  	header.removeClass('header--dark');
 	  }
   }
+
+  /* Smooth scroll to sections */
+  $('[data-scroll]').on('click', function(event){
+    event.preventDefault();
+
+    let scrollEl = $(this).data('scroll');
+    let scrollElPos = $(scrollEl).offset().top;
+
+    $('html, body').animate({
+      scrollTop: scrollElPos - headerH
+    }, 500)
+  });
+
+  /* ScrollSpy */
+  let windowH = $(window).height();
+  scrollSpy(scrollTop);
+
+
+  $(window).on("scroll", function() {
+    scrollTop = $(this).scrollTop();
+    
+    scrollSpy(scrollTop);
+ });
+
+  function scrollSpy(scrollTop) {
+    $("[data-scrollspy]").each(function() {
+      let $this = $(this);
+      let sectionId = $this.data('scrollspy');
+      let sectionOffset = $this.offset().top;
+      sectionOffset = sectionOffset - (windowH * 0.2);
+
+      if(scrollTop >= sectionOffset) {
+        $('#header__menu [data-scroll]').removeClass('active');
+
+        $('#header__menu [data-scroll="' + sectionId + '"]').addClass('active');
+      }
+
+      if(scrollTop == 0) {
+        $('#header__menu [data-scroll]').removeClass('active');
+      }
+    });
+  }
+
+
 });
